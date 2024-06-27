594fafc68ef760ae82adcdaadb58ef2260075038
tanmay4l <mrspot67@gmail.com>
2024-06-27 15:17:32 +0530

    Update x.sol

--- x.sol
+++ x.sol
@@ -1,103 +1,121 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract XContract {
-// 3 g varible
-    uint public userCount = 0;
-    uint public tweetCount = 0;
-    uint public messageCount = 0;
-
-    struct User {
-        uint userId;
-        string username;
-        uint followers;
-        mapping(uint => bool) following;
-    }
-
+contract TweetContract {
     struct Tweet {
-        uint tweetId;
-        uint userId;
-        string message;
-        uint timestamp;
-    }
-
-    struct Message {
-        uint messageId;
-        uint senderId;
-        uint receiverId;
+        address author;
         string content;
         uint timestamp;
     }
 
-    mapping(uint => User) public users;
-    mapping(uint => Tweet) public tweets;
-    mapping(uint => Message) public messages;
-    mapping(address => uint) public userAddresses;
- // event is for an annoce
-    event NewTweet(uint tweetId, uint userId, string message, uint timestamp);
-    event NewMessage(uint messageId, uint senderId, uint receiverId, string content, uint timestamp);
-    event FollowUser(uint followerId, uint followedId);
+    struct Message {
+        address sender;
+        address receiver;
+        string content;
+        uint timestamp;
+    }
 
-    modifier onlyUser() {
-        require(userAddresses[msg.sender] > 0, "You need to register first.");
+    mapping(address => bool) public authorizedPosters;
+    mapping(address => bool) public authorizedMessengers;
+    mapping(address => mapping(address => bool)) public followedUsers;
+    mapping(address => Tweet[]) public userTweets;
+    mapping(address => Message[]) public userMessages;
+
+    event TweetPosted(address indexed author, string content, uint timestamp);
+    event MessageSent(address indexed sender, address indexed receiver, string content, uint timestamp);
+    event UserFollowed(address indexed follower, address indexed followed);
+
+    modifier onlyAuthorizedPoster() {
+        require(authorizedPosters[msg.sender], "You are not authorized to post tweets.");
         _;
     }
-// singup
-    function addUser(string memory _username) public {
-        require(userAddresses[msg.sender] == 0, "User already registered.");
-        userCount++;
-        users[userCount].userId = userCount;
-        users[userCount].username = _username;
-        users[userCount].followers = 0;
-        userAddresses[msg.sender] = userCount;
+
+    modifier onlyAuthorizedMessenger() {
+        require(authorizedMessengers[msg.sender], "You are not authorized to send messages.");
+        _;
     }
 
-    function postTweet(string memory _message) public onlyUser {
-        uint userId = userAddresses[msg.sender];
-        tweetCount++;
-        tweets[tweetCount] = Tweet(tweetCount, userId, _message, block.timestamp);
-        emit NewTweet(tweetCount, userId, _message, block.timestamp);
+    function authorizePoster(address _user) public {
+        authorizedPosters[_user] = true;
     }
 
-    function sendMessage(uint _receiverId, string memory _content) public onlyUser {
-        uint senderId = userAddresses[msg.sender];
-        require(_receiverId > 0 && _receiverId <= userCount, "Receiver does not exist.");
-        messageCount++;
-        messages[messageCount] = Message(messageCount, senderId, _receiverId, _content, block.timestamp);
-        emit NewMessage(messageCount, senderId, _receiverId, _content, block.timestamp);
+    function revokePosterAuthorization(address _user) public {
+        authorizedPosters[_user] = false;
     }
 
-    function followUser(uint _followedId) public onlyUser {
-        uint followerId = userAddresses[msg.sender];
-        require(_followedId > 0 && _followedId <= userCount, "User does not exist.");
-        require(followerId != _followedId, "You cannot follow yourself.");
-        require(!users[followerId].following[_followedId], "Already following this user.");
-        users[followerId].following[_followedId] = true;
-        users[_followedId].followers++;
-        emit FollowUser(followerId, _followedId);
+    function authorizeMessenger(address _user) public {
+        authorizedMessengers[_user] = true;
     }
 
-    function getTweet(uint _tweetId) public view returns (uint, uint, string memory, uint) {
-        require(_tweetId > 0 && _tweetId <= tweetCount, "Tweet does not exist.");
-        Tweet memory tweet = tweets[_tweetId];
-        return (tweet.tweetId, tweet.userId, tweet.message, tweet.timestamp);
+    function revokeMessengerAuthorization(address _user) public {
+        authorizedMessengers[_user] = false;
     }
 
-    function getUser(uint _userId) public view returns (uint, string memory, uint) {
-        require(_userId > 0 && _userId <= userCount, "User does not exist.");
-        User storage user = users[_userId];
-        return (user.userId, user.username, user.followers);
+    function postTweet(string memory _content) public onlyAuthorizedPoster {
+        Tweet memory newTweet = Tweet(msg.sender, _content, block.timestamp);
+        userTweets[msg.sender].push(newTweet);
+        emit TweetPosted(msg.sender, _content, block.timestamp);
     }
 
-    function getMessage(uint _messageId) public view returns (uint, uint, string memory, uint) {
-        require(_messageId > 0 && _messageId <= messageCount, "Message does not exist.");
-        Message memory message = messages[_messageId];
-        return (message.messageId, message.senderId, message.content, message.timestamp);
+    function sendMessage(address _receiver, string memory _content) public onlyAuthorizedMessenger {
+        require(_receiver != address(0), "Invalid receiver address.");
+        Message memory newMessage = Message(msg.sender, _receiver, _content, block.timestamp);
+        userMessages[msg.sender].push(newMessage);
+        emit MessageSent(msg.sender, _receiver, _content, block.timestamp);
     }
 
-    function isFollowing(uint _userId, uint _followedId) public view returns (bool) {
-        require(_userId > 0 && _userId <= userCount, "User does not exist.");
-        require(_followedId > 0 && _followedId <= userCount, "Followed user does not exist.");
-        return users[_userId].following[_followedId];
+    function followUser(address _followedUser) public {
+        require(_followedUser != address(0), "Invalid user address.");
+        followedUsers[msg.sender][_followedUser] = true;
+        emit UserFollowed(msg.sender, _followedUser);
+    }
+
+    function getLatestTweets(uint _count) public view returns (Tweet[] memory) {
+        require(_count > 0, "Count must be greater than zero.");
+        uint totalTweets = userTweets[msg.sender].length;
+        uint startIndex = (totalTweets > _count) ? totalTweets - _count : 0;
+        uint endIndex = totalTweets;
+        Tweet[] memory result = new Tweet[](endIndex - startIndex);
+        for (uint i = startIndex; i < endIndex; i++) {
+            result[i - startIndex] = userTweets[msg.sender][i];
+        }
+        return result;
+    }
+
+    function getLatestUserTweets(address _user, uint _count) public view returns (Tweet[] memory) {
+        require(_user != address(0), "Invalid user address.");
+        require(_count > 0, "Count must be greater than zero.");
+        uint totalTweets = userTweets[_user].length;
+        uint startIndex = (totalTweets > _count) ? totalTweets - _count : 0;
+        uint endIndex = totalTweets;
+        Tweet[] memory result = new Tweet[](endIndex - startIndex);
+        for (uint i = startIndex; i < endIndex; i++) {
+            result[i - startIndex] = userTweets[_user][i];
+        }
+        return result;
     }
 }
+
+
+// assingment : 
+
+// You are tasked with designing a Solidity smart contract called   that simulates a basic social media platform's functionalities. The contract should allow users to post tweets, send messages, follow other users, and manage access permissions. Implement the following:
+//  TweetContract
+// 1.
+// Tweeting:
+// Create a function to allow users to post a tweet. This function should store the tweet's content, the author's address, and the timestamp of creation.
+// 2.
+// Messaging:
+// Implement a function that enables users to send messages to other users. It should record the sender, recipient, message content, and creation timestamp.
+// 3.
+// Following Users:
+// Develop a function that enables a user to follow another user by adding their address to a list of followed users.
+// 4.
+// Access Control:
+// Implement mechanisms to control access:
+// Allow users to authorize other users to post tweets or send messages on their behalf. Allow users to revoke these authorizations.
+// 5.
+// Retrieving Data:
+// Create functions to retrieve:
+// The latest tweets posted by users, considering a specified count. The latest tweets of a specific user, considering a specified count.
+ 
